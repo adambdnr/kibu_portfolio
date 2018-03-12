e64b214c2f9810a2daec782b9ca5edbcd6d0c25f
adambdnr <adambodnar2@gmail.com>
2018-03-12 20:33:29 +0100

    half done, main page good

--- static/js/script.js
+++ static/js/script.js
@@ -3,41 +3,26 @@ $(document).ready(function(){
 
     $(".menu-button").click(function () {
         if (nav.hasClass("hide")) {
-            console.log("van hide")
             nav.removeClass("hide")
             nav.addClass("show")
         } else if (nav.hasClass("show")) {
-            console.log("van show")
             nav.removeClass("show")
             nav.addClass("hide")
-        }
-        else {
-            console.log("semmi nincs")
-            nav.addClass("show")
         }
 })
 });
 
-/*function typeEffect(element, speed) {
-	var text = $(element).text();
-	$(element).html('');
-
-	var i = 0;
-	var timer = setInterval(function() {
-					if (i < text.length) {
-						$(element).append(text.charAt(i));
-						i++;
-					} else {
-						clearInterval(timer);
-					}
-				}, speed);
+$(window).scroll(function(){
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        $('#upButton').css('display', 'block');
+        $('#upButton').css('opacity', 1);
+    } else {
+        $('#upButton').css('display', 'none');
+        $('#upButton').css('opacity', 0);
+    }
 }
+);
 
-$( document ).ready(function() {
-  var speed = 75;
-  var delay = $('.header-image > p:nth-child(1)').text().length * speed + speed;
-  typeEffect($('.header-image > p:nth-child(1)'), speed);
-  setTimeout(function(){
-    typeEffect($('.header-image > p:nth-child(2)'), speed);
-  }, delay);
-});*/
+function goToTop() {
+    $(window).scrollTop(0);
+}
